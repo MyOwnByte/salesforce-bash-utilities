d0e8d74d8bab255d9be25599c02cacd4f1bb511e
myownbyte <myownbyte@gmail.com>
2021-11-30 17:05:56 -0300

    Modified line return to unix

--- bash-scripts/delete-all-logs/delete-all-logs.sh
+++ bash-scripts/delete-all-logs/delete-all-logs.sh
@@ -1,2 +1,3 @@
-sfdx force:data:soql:query -q "SELECT Id FROM ApexLog" -r csv > ./bash-scripts/delete-all-logs/apexlog.csv
-sfdx force:data:bulk:delete -s ApexLog -f ./bash-scripts/delete-all-logs/apexlog.csv
+#!/bin/bash
+sfdx force:data:soql:query -q "SELECT Id FROM ApexLog" -r csv > log/apexlog.csv
+sfdx force:data:bulk:delete -s ApexLog -f log/apexlog.csv
